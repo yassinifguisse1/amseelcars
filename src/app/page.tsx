241437d6c6badf3b2aba746dd695aa8aa0068bb0
Yassine Ifguisse <yassinifguisse100@gmail.com>
2025-08-12 19:06:34 +0200

    Add new images, update dependencies, and implement ZoomParallax component with styling adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+'use client'
+import { useEffect } from 'react';
+import ZoomParallax from "@/components/ZoomParallax";
+import Lenis from 'lenis';
 
 
 export default function Home() {
+
+  useEffect( () => {
+    const lenis = new Lenis()
+   
+    function raf(time: number) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+},[])
   return (
    
-    <div className="page-content hero">
-      <h1>Hello <sup>01</sup></h1>
+    <div className="page-content hero" >
+      <ZoomParallax />
+      
     </div>
   );
 }
